dc2e93701b81a6e48e59f9eb18a210dcb46153b2
Renato D. Santos Jr <renatodsantosjr9@gmail.com>
2026-02-01 19:10:48 +0800

    "feat(dashboard): added add goals, not yet finished missing datepicker"

--- frontend/app/dashboard/add_goals.tsx
+++ frontend/app/dashboard/add_goals.tsx
@@ -1,9 +1,48 @@
-import { Text, View } from 'react-native'
+import { Button } from '@/components/ui/Button';
+import { Input } from '@/components/ui/Input';
+import { Label } from '@/components/ui/Label';
+import CustomSelect from '@/components/ui/Picker';
+import { useState } from 'react';
+import { Text, View } from 'react-native';
 
 const AddGoals = () => {
+    const [value, setValue] = useState("");
+
     return (
-        <View>
-            <Text>AddGoals</Text>
+        <View className='px-6 pb-8 pt-9 flex-1 flex-col justify-between'>
+            <View className='flex-col gap-3'>
+
+                <View className='flex-col gap-1'>
+                    <Label>Goal Title</Label>
+                    <Input />
+                </View>
+
+                <View className='flex-col gap-1'>
+                    <Label>Amount</Label>
+                    <Input />
+                </View>
+
+                <View className='flex-col gap-1'>
+                    <Label>Contribution Type</Label>
+                    <CustomSelect options={[
+                        { label: "Daily", value: "daily" },
+                        { label: "Weekly", value: "weekly" },
+                        { label: "Monthly", value: "monthly" },
+                        { label: "Yearly", value: "yearly"}
+                    ]} />
+                </View>
+
+                <View className='flex-col gap-1'>
+                    <Label>Deadline</Label>
+                    <Input />
+                </View>
+            </View>
+
+            <Button>
+                <Text className='text-white text-xl font-bold font-sans-bold'>
+                    ADD GOAL
+                </Text>
+            </Button>
         </View>
     )
 }
